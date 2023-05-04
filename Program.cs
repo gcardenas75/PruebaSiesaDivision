56c64888fe1c52a9aef60292262e331858279237
gcardenas75 <grcv.91@gmail.com>
2023-05-03 22:25:59 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using PruebaTecnica.Operacion;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       Division divisionNueva = new Division(); 
+        Console.WriteLine("El resultado de la division es: ");
+        Console.WriteLine(divisionNueva.division(10, 2));
+    }
+}
